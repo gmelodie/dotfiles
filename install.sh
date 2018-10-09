b0505f6c745e2b32f51a36e2e0599e62ab527f9f
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-09 20:29:25 -0300

    Create dir if not exists

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 
+# neovim
+if [ ! -d "~/.config" ]
+then
+    mkdir ~/.config
+fi
+
+mkdir ~/.config/nvim
+cp init.vim ~/.config/nvim
+
+
 # zsh
 sed -e "s/USER/$USER/" zshrc > zshrc-temp # change USER for the current user
 chsh -s $(which zsh)
 mv zshrc-temp ~/.zshrc
 
+
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# neovim
-mkdir ~/.config/nvim
-cp init.vim ~/.config/nvim
 
