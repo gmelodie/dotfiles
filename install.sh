9a8e683b0e5eaf12bcc9f4b9bbaa5713583df112
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-09 20:21:41 -0300

    Correct temp file name

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 
 # zsh
-sed -e "s/USER/$USER/" zshrc > .zshrc # change USER for the current user
+sed -e "s/USER/$USER/" zshrc > zshrc-temp # change USER for the current user
 chsh -s $(which zsh)
-mv .zshrc ~/.zshrc
+mv zshrc-temp ~/.zshrc
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
