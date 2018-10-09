0a4d52e63a43868573b6c6db2e1ef0ac9a205ee6
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-09 18:42:37 -0300

    Add neovim to debian-install

--- debian-install.sh
+++ debian-install.sh
@@ -1,6 +1,12 @@
 
-
 # zsh
 sudo apt-get install zsh
 chsh -s $(which zsh)
 cp zshrc ~/.zshrc
+
+# neovim
+sudo apt-get install neovim
+sudo apt-get install python3-neovim
+mkdir ~/.config/nvim
+cp init.vim ~/.config/nvim
+
