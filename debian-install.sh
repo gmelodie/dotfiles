d4b604037584e46bca1b97d36b3796ca17783fe0
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-09 18:45:53 -0300

    Add oh-my-zsh

--- debian-install.sh
+++ debian-install.sh
@@ -4,6 +4,9 @@ sudo apt-get install zsh
 chsh -s $(which zsh)
 cp zshrc ~/.zshrc
 
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # neovim
 sudo apt-get install neovim
 sudo apt-get install python3-neovim
