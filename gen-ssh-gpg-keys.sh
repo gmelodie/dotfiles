ba489eb35d8a8f7deff08bc1027be96e121323b1
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2021-05-18 22:30:45 -0300

    Fix key generation

--- gen-ssh-gpg-keys.sh
+++ gen-ssh-gpg-keys.sh
@@ -2,15 +2,24 @@ stty sane # prevent the ^M on enter issue
 read -p "Name: " name
 read -p "Email: " email
 
-echo "\n-----------------------  SSH KEY  ------------------------"
+echo "-----------------------  SSH KEY  ------------------------"
+
 echo "\n" | ssh-keygen -t ed25519 -C "$email" -N "" > /dev/null
+
 eval "$(ssh-agent -s)" > /dev/null
+
 ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
+
 cat ~/.ssh/id_ed25519.pub
 
-echo "\n-----------------------  GPG KEY  ------------------------"
-gpg --quick-generate-key "$name <$email>" rsa4096 default never
-gpgkey=$(gpg --list-secret-keys --keyid-format LONG | grep -B 2 $name | grep 'sec\s*rsa4096' | sed 's/\//\ /g' | awk -F " " 'NR==1{print $3}')
-gpg --armor --export $gpgkey
-git config --global user.signingkey $gpgkey
-echo "----------------------------------------------------------\n"
+echo "-----------------------  GPG KEY  ------------------------"
+
+gpg --quick-generate-key "$name <$email>" rsa4096 default never > /dev/null
+
+gpgkey=$(gpg --list-secret-keys --keyid-format LONG | grep -B 2 $name | grep 'sec\s*rsa4096' | sed 's/\//\ /g' | awk -F " " 'NR==1{print $3}') > /dev/null 2>&1
+
+gpg --armor --export $gpgkey > /dev/null
+
+git config --global user.signingkey $gpgkey > /dev/null
+
+echo "----------------------------------------------------------"
--- install.sh
+++ install.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
+echo -n 'Starting full system update...'
 
 # Install dependencies (requires apt)
-sudo apt -y update && sudo apt -y upgrade
-sudo apt install curl build-essential git python3 python3-neovim python3-virtualenvwrapper golang zsh exuberant-ctags gnome-terminal fzf nodejs dconf
+sudo apt -y update > /dev/null 2>&1 && sudo apt -y upgrade > /dev/null 2>&1
+sudo apt install curl build-essential git python3 python3-neovim python3-virtualenvwrapper golang zsh exuberant-ctags gnome-terminal fzf nodejs dconf > /dev/null 2>&1
 
+echo -e "${GREEN}Done${NC}"
 
-echo 'Installing dotfiles...'
 
 # --------------------- NVIM ------------------------
-echo 'Installing Neovim configurations (appimage, init.vim)'
-mkdir -p $HOME/.nvim
-wget -O $HOME/.nvim/nvim.appimage "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
-chmod +x $HOME/.nvim/nvim.appimage
+echo -n 'Installing Neovim configurations (appimage, init.vim)...'
+mkdir -p $HOME/.nvim > /dev/null 2>&1
+wget -O $HOME/.nvim/nvim.appimage "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage" > /dev/null 2>&1
+chmod +x $HOME/.nvim/nvim.appimage > /dev/null 2>&1
 
 mkdir -p $HOME/.config/nvim
 if [ -e $HOME/.config/nvim/init.vim ]
 then
-    echo '[Warning] Found existent init.vim, moving to init.vim.old'
+    echo -e "\n${ORANGE}[Warning] Found existent init.vim, moving to init.vim.old${NC}"
     mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.old
 fi
 
 ln -sf $BASEDIR/init.vim $HOME/.config/nvim/init.vim
 
+echo -e "${GREEN}Done${NC}"
 
 
 # --------------------- SSH and GPG ------------------------
@@ -32,27 +38,41 @@ echo 'Generating SSH and GPG keys'
 $BASEDIR/gen-ssh-gpg-keys.sh
 
 # --------------------- GIT ------------------------
-echo 'Installing Git configurations (.gitconfig)'
+echo -n 'Installing Git configurations (.gitconfig)...'
 ln -sf $BASEDIR/gitconfig $HOME/.gitconfig
+echo -e "${GREEN}Done${NC}"
 
 
 # --------------------- XCREEP ------------------------
-echo 'Installing xcreep'
-if ! command -v go &> /dev/null
+echo -n 'Installing xcreep...'
+if ! command -v go &> /dev/null 2>&1
 then
-    echo "[Error] Golang not found, skipping xcreep installation"
+    echo "\n${RED}[Error] Golang not found, skipping xcreep installation${NC}"
 else
-    go get github.com/gmelodie/xcreep
+    go get github.com/gmelodie/xcreep > /dev/null 2>&1
+    echo -e "${GREEN}Done${NC}"
 fi
 
 # --------------------- GNOME PREFERENCES ------------------------
-echo 'Loading gnome-terminal preferences'
+echo -n 'Loading gnome-terminal preferences...'
 if ! command -v gnome-terminal &> /dev/null
 then
-    echo "[Error] gnome-terminal not found, skipping preferences installation"
+    echo "\n${RED}[Error] gnome-terminal not found, skipping preferences installation${NC}"
 else
     cat $BASEDIR/gterminal.preferences | dconf load /org/gnome/terminal/legacy/profiles:/
+    echo -e "${GREEN}Done${NC}"
 fi
+
+# --------------------- DESKTOP ICONS ------------------------
+# Symlink application desktop icons
+ln -sf $BASEDIR/applications/* $HOME/.local/share/applications
+
+
+# --------------------- GIT REMOTE HTTPS -> SSH  ------------------------
+echo -n 'Changing dotfiles remote from HTTPS to SSH...'
+git remote set-url origin git@github.com:gmelodie/dotfiles.git
+echo -e "${GREEN}Done${NC}"
+
 
 # --------------------- ZSH ------------------------
 echo 'Installing Zsh configurations (zshrc)'
@@ -66,16 +86,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # copy zsh configuration file
 if [ -e $HOME/.zshrc ]
 then
-    echo '[Warning] Found existent zshrc, moving to zshrc.old'
+    echo -e "\n${ORANGE}[Warning] Found existent zshrc, moving to zshrc.old${NC}"
     mv $HOME/.zshrc $HOME/zshrc.old
 fi
 
 ln -sf $BASEDIR/zshrc $HOME/.zshrc
-
-
-# --------------------- DESKTOP ICONS ------------------------
-# Symlink application desktop icons
-ln -sf $BASEDIR/applications/* $HOME/.local/share/applications
 
 
 echo 'All done!'
