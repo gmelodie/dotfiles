7fafb6bebc941ba91aa8185faf36c898f40901d6
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2023-11-29 15:04:51 -0300

    Add obsidian install script

---
+++ scripts/install-obsidian.sh
@@ -0,0 +1,5 @@
+TAG_VERSION=$(curl -s -I https://github.com/obsidianmd/obsidian-releases/releases/latest | grep -i location | cut -d "/" -f 8 | cut -d "v" -f 2 | tr -d '\r')
+DOWNLOAD_URL="https://github.com/obsidianmd/obsidian-releases/releases/download/v${TAG_VERSION}/obsidian_${TAG_VERSION}_amd64.snap"
+
+wget -P "$HOME/Downloads" $DOWNLOAD_URL
+sudo snap install "$HOME/Downloads/obsidian_${TAG_VERSION}_amd64.snap" --dangerous --classic
