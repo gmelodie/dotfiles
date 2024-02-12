2cfeab69ccc19bc5b16ad3edab0b753969170e28
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-02-11 21:32:19 -0300

    Add install nvim script

---
+++ scripts/nvim-update.sh
@@ -0,0 +1,4 @@
+mkdir -p $HOME/.nvim 2>&1
+wget -O $HOME/.nvim/nvim.appimage "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage" 2>&1
+chmod +x $HOME/.nvim/nvim.appimage 2>&1
+
