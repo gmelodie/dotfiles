34481a9dd98cd1ceebd93609db0c4019e9ae1ede
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-06-07 10:58:51 -0300

    update gen ssh script

--- gen-ssh-gpg-keys.sh
+++ gen-ssh-gpg-keys.sh
@@ -1,25 +1,31 @@
 stty sane # prevent the ^M on enter issue
 read -p "Your Name: " name
 read -p "Your Email: " email
+KEY_TYPE=ed25519
 
 echo "-----------------------  SSH KEY  ------------------------"
 
-echo "\n" | ssh-keygen -C "$email" -N ""
+ssh-keygen -t $KEY_TYPE -f ~/.ssh/id_$KEY_TYPE -C "$email" -N ""
 
 eval "$(ssh-agent -s)"
 
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_$KEY_TYPE
 
-cat ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_$KEY_TYPE.pub
 
 echo "-----------------------  GPG KEY  ------------------------"
 
-gpg --quick-generate-key "$name <$email>" rsa4096 default never
+gpg --quick-generate-key "$name <$email>" $KEY_TYPE default never
 
-gpgkey=$(gpg --list-secret-keys --keyid-format LONG | grep -B 2 $name | grep 'sec\s*rsa4096' | sed 's/\//\ /g' | awk -F " " 'NR==1{print $3}')
+gpgkeyid=$(gpg --list-secret-keys --keyid-format LONG | grep -B 2 "$name" | grep "sec\s*$KEY_TYPE" | sed 's/\//\ /g' | awk -F " " 'NR==1{print $3}')
 
-gpg --armor --export $gpgkey
+gpg --armor --export $gpgkeyid
 
-git config --global user.signingkey $gpgkey
+gpgfingerprint=$(gpg --list-secret-keys | grep $gpgkeyid | sed 's/\s//g')
+
+cat > ~/.gitconfig.local <<EOF
+[user]
+	signingkey = $gpgfingerprint
+EOF
 
 echo "----------------------------------------------------------"
