8289e61075b07018219c2c4e18fbdfbc512e5936
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-06-07 11:24:44 -0300

    update and cleanup install script

--- install.sh
+++ install.sh
@@ -46,8 +46,7 @@ function install() {
     echo '############# Starting full system upgrade...'
 
     sudo apt -y update > /dev/null && sudo apt -y upgrade > /dev/null
-    sudo apt install -y curl build-essential git python3 python3-neovim python3-virtualenvwrapper golang zsh universal-ctags gnome-terminal fzf nodejs tmux golang-go clang clangd fuse libfuse2 ripgrep
-    sudo modprobe -v fuse
+    sudo apt install -y curl build-essential git python3 python3-neovim golang zsh universal-ctags gnome-terminal fzf nodejs tmux golang-go clang clangd ripgrep
 
     # install meslo fonts
     mkdir -p ~/.fonts
@@ -120,18 +119,6 @@ function config() {
 
     echo -n 'Zsh configurations (.zshrc)...'
     link_config_file $BASEDIR/zshrc $HOME/.zshrc
-    link_config_file $BASEDIR/p10k.zsh $HOME/.p10k.zsh
-
-    echo -n 'Tmux configurations (.tmux.conf)...'
-    link_config_file $BASEDIR/tmux.conf $HOME/.tmux.conf
-
-    echo -n 'Alacritty configurations (alacritty.toml)...'
-    mkdir -p $HOME/.config/alacritty
-    link_config_file $BASEDIR/alacritty.toml $HOME/.config/alacritty/alacritty.toml
-
-    echo -n 'Adding alacritty as terminal alternative (default)...'
-    sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which alacritty` 50
-    sudo update-alternatives --config x-terminal-emulator
 
     echo -n 'Loading gnome-terminal preferences...'
     if ! command -v gnome-terminal &> /dev/null
@@ -141,11 +128,6 @@ function config() {
         cat $BASEDIR/gterminal.preferences | dconf load /org/gnome/terminal/legacy/profiles:/
         echo -e "${GREEN}Done${NC}"
     fi
-
-    echo -n 'Copying desktop icons...'
-    # Symlink application desktop icons
-    ln -sf $BASEDIR/applications/* $HOME/.local/share/applications
-    echo -e "${GREEN}Done${NC}"
 
 } # --------------------- END Configuring ------------------------
 
