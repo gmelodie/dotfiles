ec27fc4b0bfd9a412bcafaa632d1df95f1cc0fcf
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-06-17 14:35:15 -0300

    add mouse scrolling st

--- suckless/st/st.h
+++ suckless/st/st.h
@@ -19,6 +19,7 @@
 
 #define TRUECOLOR(r,g,b)	(1 << 24 | (r) << 16 | (g) << 8 | (b))
 #define IS_TRUECOL(x)		(1 << 24 & (x))
+#define HISTSIZE            2000
 
 enum glyph_attribute {
 	ATTR_NULL       = 0,
@@ -88,6 +89,7 @@ void sendbreak(const Arg *);
 void toggleprinter(const Arg *);
 
 int tattrset(int);
+int tisaltscr(void);
 void tnew(int, int);
 void tresize(int, int);
 void tsetdirtattr(int);
