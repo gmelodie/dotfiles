0d7087b0ed88fd02c94d304b6affeceaa2f91650
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-06-21 10:04:54 -0300

    add wikiman, ranger

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ function install_debian() {
 function install_archlinux() {
     echo '############# Starting full system upgrade (Pacman)...'
     sudo pacman -Syu
-    sudo pacman -S curl build-essential base-devel git python python-pynvim python-pip, python-pipxgo zsh universal-ctags fzf nodejs tmux clang clang-analyzer ripgrep neovim mesa ly libx11 libxft xorg-server xorg-xinit terminus-font wget xorg-xauth xorg-apps openssh feh imagemagick pipewire pipewire-pulse pipewire-alse pipewire-jack pipewire-audio wireplumber alsa-utils alsa-firmware bluez bluez-utils bluez-deprecated-tools brightnessctl blueman xclip playerctl
+    sudo pacman -Sy curl build-essential base-devel git python python-pynvim python-pip, python-pipxgo zsh universal-ctags fzf nodejs tmux clang clang-analyzer ripgrep neovim mesa ly libx11 libxft xorg-server xorg-xinit terminus-font wget xorg-xauth xorg-apps openssh feh imagemagick pipewire pipewire-pulse pipewire-alse pipewire-jack pipewire-audio wireplumber alsa-utils alsa-firmware bluez bluez-utils bluez-deprecated-tools brightnessctl blueman xclip playerctl wikiman arch-wiki-docs ranger
     systemctl --user enable --now pipewire pipewire-pulse wireplumber
     sudo systemctl enable --now bluetooth
     pipx install pywal
