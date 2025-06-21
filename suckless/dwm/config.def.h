b990992d88a5b5daa88eb3d1fbf273890baee691
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-06-21 12:02:55 -0300

    fix dwm status bar

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
@@ -12,29 +12,44 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=18" };
-static const char dmenufont[]       = "Hack Nerd Font:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-	[SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-	[SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-	[SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+static const char dmenufont[]       = "Hack Nerd Font:size=18";
+
+// Opacity levels
+static const unsigned int baralpha = 0xb0;      // opacity
+static const unsigned int borderalpha = OPAQUE; // Fully opaque (make sure OPAQUE is defined as 0xff)
+
+/* default colors used if xrdb is not loaded */
+static const char normbgcolor[]           = "#076678"; // blue
+static const char selbgcolor[]            = "#b57614"; // yellow
+
+static const char normfgcolor[]           = "#282828"; // black
+static const char selfgcolor[]            = "#282828";
+static const char normbordercolor[]       = "#282828";
+static const char selbordercolor[]        = "#fe8019"; // orange
+
+static const char *colors[][3] = {
+       /*               fg           bg           border   */
+		[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+		[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+		/* for bar --> {text, background, null} */
+        [SchemeStatus]  = { normfgcolor, normbgcolor,  normbgcolor }, /* status R */
+		[SchemeTagsSel]  = { selfgcolor, selbgcolor, selbgcolor }, /* tag L selected */
+		[SchemeTagsNorm]  = { normfgcolor, normbgcolor,  normbgcolor }, /* tag L unselected */
+		[SchemeInfoSel]  = { selfgcolor, selbgcolor, selbgcolor  }, /* info M selected */
+		[SchemeInfoNorm]  = { normfgcolor, normbgcolor,  normbgcolor  }, /* info M unselected */
 };
-static const unsigned int alphas[][3]      = {
-    /*               fg      bg        border*/
+
+// Transparency settings: fg, bg, border (values: 0x00 transparent to 0xff opaque)
+static const unsigned int alphas[][3] = {
     [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeSel] = { OPAQUE, baralpha, borderalpha },
+    [SchemeStatus]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeTagsSel]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeTagsNorm]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeInfoSel]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeInfoNorm]  = { OPAQUE, baralpha, borderalpha },
 };
+
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -104,7 +119,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normbordercolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
